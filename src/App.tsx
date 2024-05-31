2f41357822a799f2d611fe09041787f065ec10a8
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-31 04:09:01 -0300

    feat: adding config routes and toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { Flip, ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 import './App.css'
+import { RoutesMain } from './routes/routes'
 
 function App() {
   return (
     <>
+      <RoutesMain />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Flip}
+        />
     </>
   )
 }
