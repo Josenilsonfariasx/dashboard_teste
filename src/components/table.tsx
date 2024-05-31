9c9bd7aa0767d7b25abe35e2b0fd437a13ebbfcf
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-31 04:41:19 -0300

    fix: responsive table for mobile

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -8,23 +8,23 @@ interface TableProps {
 
 export const Table: React.FC<TableProps> = ({ users, authenticatedUserId }) => {
   return (
-    <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
+    <div className="shadow-lg rounded-lg overflow-hidden md:mx-10">
       <table className="w-full table-fixed">
         <thead>
           <tr className="bg-gray-100">
-            <th className="w-1/4 py-4 px-6 text-center text-gray-600 font-bold uppercase">Nome</th>
-            <th className="w-1/4 py-4 px-6 text-center text-gray-600 font-bold uppercase">Email</th>
-            <th className="w-1/4 py-4 px-6 text-center text-gray-600 font-bold uppercase">Email Verificado</th>
-            <th className="w-1/4 py-4 px-6 text-center text-gray-600 font-bold uppercase">Você</th>
+            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Nome</th>
+            <th className="w-2/5 py-4  text-center text-gray-600 font-bold ">Email</th>
+            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Validou</th>
+            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Você</th>
           </tr>
         </thead>
         <tbody className="bg-white">
           {users.map((user) => (
             <tr key={user.id} className={`border-b border-gray-200`}>
               <td className="py-4 px-6 text-center text-gray-500">{user.name}</td>
-              <td className="py-4 px-6 text-center text-gray-500 truncate">{user.email}</td>
+              <td className="py-4 px-6 text-center text-gray-500">{user.email}</td>
               <td className="py-4 px-6 text-center text-gray-500">
-                {user.email_verified_at ? 'Verified' : 'Not Verified'}
+                {user.email_verified_at ? 'Sim' : 'Não'}
               </td>
               <td className="py-4 px-6 text-center text-gray-500">
                 {user.id === authenticatedUserId ? '✅' : ''}
