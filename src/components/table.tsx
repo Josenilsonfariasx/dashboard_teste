8460ddf2f35fa626722ddc2a33fa16a24d9674d1
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-31 04:52:57 -0300

    fix: responsive text

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -12,21 +12,21 @@ export const Table: React.FC<TableProps> = ({ users, authenticatedUserId }) => {
       <table className="w-full table-fixed">
         <thead>
           <tr className="bg-gray-100">
-            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Nome</th>
-            <th className="w-2/5 py-4  text-center text-gray-600 font-bold ">Email</th>
-            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Validou</th>
-            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Você</th>
+            <th className="w-1/5 py-4  text-center text-gray-600 font-bold ">Nome</th>
+            <th className="w-1/4 py-4  text-center text-gray-600 font-bold ">Email</th>
+            <th className="w-1/6 py-4  text-center text-gray-600 font-bold ">Validou</th>
+            <th className="w-1/6 py-4  text-center text-gray-600 font-bold ">Você</th>
           </tr>
         </thead>
         <tbody className="bg-white">
           {users.map((user) => (
             <tr key={user.id} className={`border-b border-gray-200`}>
-              <td className="py-4 px-6 text-center text-gray-500">{user.name}</td>
-              <td className="py-4 px-6 text-center text-gray-500">{user.email}</td>
-              <td className="py-4 px-6 text-center text-gray-500">
+              <td className="py-4 text-center text-gray-500">{user.name}</td>
+              <td className="py-4 text-center text-gray-500 truncate">{user.email}</td>
+              <td className="py-4 text-center text-gray-500">
                 {user.email_verified_at ? 'Sim' : 'Não'}
               </td>
-              <td className="py-4 px-6 text-center text-gray-500">
+              <td className="py-4 text-center text-gray-500">
                 {user.id === authenticatedUserId ? '✅' : ''}
               </td>
             </tr>
